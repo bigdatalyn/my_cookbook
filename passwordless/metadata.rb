f9231d9b7a795cc87f6278eca2fc4e2c73c6edba
bigdatalyn <88322511@qq.com>
2016-06-12 20:33:27 +0800

    Update metadata.rb

--- passwordless/metadata.rb
+++ passwordless/metadata.rb
@@ -1,6 +1,6 @@
 name             'passwordless'
 maintainer       'Hong Lin'
-maintainer_email ''
+maintainer_email 'bigdata_lyn@126.com'
 license          'All rights reserved'
 description      'Installs/Configures passwordless'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
